0f6f2744fd44a0136c9763f401d70d9d46c7bae9
unknown <dreicleal@hotmail.com>
2023-12-18 17:23:19 +0000

    Added design option
    Design options included and able to select with your img profile.

--- src/components/ticketDesign/TicketDesign.tsx
+++ src/components/ticketDesign/TicketDesign.tsx
@@ -1,7 +1,31 @@
 import { useUser } from "@/contexts/userContext";
 import Image from "next/image";
+import styles from "./index.module.scss";
+import { ticketModels } from "@/assets/ticketModels";
+import { useState } from "react";
 
 export const TicketDesign = () => {
+  const [choosedLayout, setChoosedLayout] = useState(0);
   const { croppedImg } = useUser();
-  return <Image src={croppedImg} alt="selected img area" width={250} height={250} />;
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.ticketContainer}>
+        <Image src={ticketModels[choosedLayout]} alt="ticket Layout" className={styles.ticketSelected}/>
+          <Image
+            src={croppedImg}
+            alt="selected img area"
+            width={250}
+            height={250}
+            className={styles.ticketPhoto}
+          />
+      </div>
+      <ul className={styles.selectTicketContainer}>
+        {ticketModels.map((model, i) => (
+          <li key={i} onClick={() => setChoosedLayout(i)}>
+            <Image src={model} alt={`ticket model ${i}`} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
