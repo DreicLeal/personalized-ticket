e5e897e9b19e72b2b0f5ab8a3fba465ca8f5ac4d
unknown <dreicleal@hotmail.com>
2024-01-15 17:19:11 +0000

    Generating the ticket composition on a new page
    Lib html2canvas installed, added last page with the completed
    image composition to download

--- src/components/ticketDesign/TicketDesign.tsx
+++ src/components/ticketDesign/TicketDesign.tsx
@@ -2,15 +2,33 @@ import { useUser } from "@/contexts/userContext";
 import Image from "next/image";
 import styles from "./index.module.scss";
 import { ticketModels } from "@/assets/ticketModels";
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+import html2canvas from "html2canvas";
+import { useRouter } from "next/navigation";
 export const TicketDesign = () => {
   const [choosedLayout, setChoosedLayout] = useState(0);
   const [username, setUsername] = useState("");
-  const { croppedImg } = useUser();
+
+  const { croppedImg, setImgData, imgData } = useUser();
+  const ticketContainerRef = useRef(null);
+  const router = useRouter();
+
+  const saveAsImage = async () => {
+    if (ticketContainerRef.current) {
+      const canvas = await html2canvas(ticketContainerRef.current);
+      const imgComponent = canvas.toDataURL();
+      setImgData(imgComponent);
+      console.log(imgComponent)
+    }
+  };
+
+  const lastStep = () => {
+    saveAsImage();
+    router.push("/allDone", { scroll: false });
+  };
   return (
     <div className={styles.modalOverlay}>
-      <div className={styles.ticketContainer}>
+      <div ref={ticketContainerRef} className={styles.ticketContainer}>
         <Image
           src={ticketModels[choosedLayout]}
           alt="ticket Layout"
@@ -19,8 +37,8 @@ export const TicketDesign = () => {
         <Image
           src={croppedImg}
           alt="selected img area"
-            width={250}
-            height={250}
+          width={250}
+          height={250}
           className={styles.ticketPhoto}
         />
         <p className={styles.username}>{username}</p>
@@ -39,7 +57,8 @@ export const TicketDesign = () => {
           </li>
         ))}
       </ul>
-      <button>Finalizar</button>
+      <button onClick={lastStep}>Finalizar</button>
+    {/* {imgData && <Image src={imgData} alt="ticket completo" width={170} height={340}/>} */}
     </div>
   );
 };
